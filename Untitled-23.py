6a43813a3738760c5c831cdd01add2de6f319d23
shubhamsr07 <138297941+shubhamsr07@users.noreply.github.com>
2023-07-04 18:57:36 +0530

    day 2 assessment

---
+++ Untitled-23.py
@@ -0,0 +1,24 @@
+ <!DOCTYPE html>
+<html>
+<head>
+  <title>My Webpage</title>
+  <style>
+    /* CSS styles */
+    header {
+      background-color: #87CEEB;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>Welcome to My Webpage</h1>
+  </header>
+  
+  <main>
+    <h2>Main Content</h2>
+    <p>This is the main content of the webpage.</p>
+    <!-- Add more content here as needed -->
+  </main>
+</body>
+</html>
